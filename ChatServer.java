19ed180953206c29ee3abe552ee7b5862fcd242b
bolditsin <70659316+bolditsin@users.noreply.github.com>
2020-10-09 01:17:49 +0500

    Add files via upload
    Прописан функционал кнопки Send, задан перенос сообщения в лог

---
+++ ChatServer.java
@@ -0,0 +1,12 @@
+package chatLessions;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+
+}
